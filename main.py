9ba05b9ef9b1e92a3a644b38f1cdf796628e1cd5
saisiddhanthks <saisiddhanthks@outlook.com>
2026-06-08 12:46:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pandas as pd
 
-df = pd.read_csv(
+brand = pd.read_csv(
+    "mstr_vw_ris_brand_all.csv",
+    low_memory=False
+)
+
+volume = pd.read_csv(
     "mstr_vw_fact_weekly_store_sku_volume.csv",
     nrows=10000,
     low_memory=False
 )
 
-print("Unique Brands:", df['client_brand_oid'].nunique())
-print("\nSample Brand IDs:")
-print(df['client_brand_oid'].drop_duplicates().head(20))
+brand_ids = volume['client_brand_oid'].drop_duplicates()
+
+result = brand[
+    brand['client_brand_oid'].isin(brand_ids)
+]
+
+print(result[['client_brand_oid','brand_title']].sort_values('brand_title'))
